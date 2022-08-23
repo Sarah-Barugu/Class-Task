e4710c7ca7cc1efa6fca7d41cd27fd2e6a2ba4c5
Chukwu Blessed <Zoddcommunity@gmail.com>
2022-08-23 07:32:07 -0700

    final changes

--- task2/script.js
+++ task2/script.js
@@ -5,11 +5,6 @@ let num = 0;
 for(let i = 0; i <= arr.length; i++) {
   if(arr[i] >= num) {
     num = arr[i];
-
-    // const html = `
-    //   <h1> ${num} </h1>
-    // `
-    // htmlFile2.insertAdjacentHTML('beforeend', html);
   }
 }
 document.write(`Output: ${num}`);
