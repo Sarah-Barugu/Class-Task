607763f776d85d4b342f0b309094180c18e36ac8
Chukwu Blessed <Zoddcommunity@gmail.com>
2022-08-23 07:31:39 -0700

    wriring directly to the DOM

--- task2/script.js
+++ task2/script.js
@@ -1,17 +1,15 @@
 'use strict'
 
-const htmlFile = document.querySelector('.append');
-
 const arr = [5, -2, -6, 0, 6, -1, 10, -100];
 let num = 0;
 for(let i = 0; i <= arr.length; i++) {
   if(arr[i] >= num) {
     num = arr[i];
 
-    const html = `
-      <h1> ${num} </h1>
-    `
-    htmlFile.insertAdjacentHTML('beforeend', html);
+    // const html = `
+    //   <h1> ${num} </h1>
+    // `
+    // htmlFile2.insertAdjacentHTML('beforeend', html);
   }
 }
-console.log(num);
+document.write(`Output: ${num}`);
