a95cb554f47b84a96225429e67e1d7edf05f1b57
Chukwu Blessed <Zoddcommunity@gmail.com>
2022-08-23 07:24:54 -0700

    checking for other numbers

--- task2/script.js
+++ task2/script.js
@@ -1,4 +1,10 @@
 'use strict'
 
-const arr = [5, -2, -6, 0, 6, -1];
-arr.
+const arr = [5, -2, -6, 0, 6, -1, 10, -100];
+let num = 0;
+for(let i = 0; i <= arr.length; i++) {
+  if(arr[i] >= num) {
+    num = arr[i];
+  }
+}
+console.log(num);
