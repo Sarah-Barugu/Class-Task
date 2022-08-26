0d4148f7aed40fad0ad764a00d4cdd06418a6815
Sarah-Barugu <sarahtokoni23@gmail.com>
2022-08-26 14:38:10 +0100

    added comments to the html and javascript file

--- task1/script.js
+++ task1/script.js
@@ -8,5 +8,7 @@ for(let i = 1; i <= 15; i++) {
   const html = `
     <h1> ${i % 2 === 0 ? `${i} is Even` : `${i} is Odd`} </h1>
   `
+
+  // appending html tag into the html file before the parent root tag
   htmlFile.insertAdjacentHTML('beforeend', html);
 }
